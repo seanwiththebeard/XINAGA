825c7eb2f30ae072cc22336b01263b2f57e46f62
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2022-10-13 15:59:09 -0400

    updated from 8bitworkshop.com

--- XINAGA.c
+++ XINAGA.c
@@ -1,7 +1,12 @@
+#include "xinaga.h"
+
+//#link "Graphics.c"
 
 
 void main()
 {
+  InitializeGraphics();
+  
   while (1)
   {}
 }
---
+++ xinaga.h
@@ -0,0 +1,58 @@
+#ifndef _XINAGA_H
+#define _XINAGA_H
+
+#include <stdint.h>
+typedef uint8_t byte;
+typedef enum { false, true } bool;
+
+//Graphics
+void InitializeGraphics(void);
+
+//	Drawing
+void ClearScreen(void);
+void SetChar(byte x, byte y, byte index);
+void DrawLineH(char index, byte x, byte y, byte length);
+void DrawLineV(char index, byte x, byte y, byte length);
+void DrawBorder(byte xPos, byte yPos, byte width, byte height, bool fill);
+void SetTileOrigin(byte x, byte y);
+void DrawTileFast(byte index, byte x, byte y);
+
+//	Text
+void PrintString(char text[16], byte posx, byte posy, bool fast, bool buffer);
+
+//	Buffer
+void UpdateColors(void);
+void CopyDoubleBuffer(void);
+void CopyDoubleBufferArea(byte posX, byte posY, byte sizeX, byte sizeY);
+void ReverseBufferArea(byte posX, byte posY, byte sizeX, byte sizeY);
+
+//	Scrolling
+void ScrollReset(void);
+void MoveScreenUp(void);
+void MoveScreenDown(void);
+
+//	Debug
+void DrawCharacterSet(byte destX, byte destY);
+
+//Audio
+extern void PlaySong(void);
+extern void StopSong(void);
+
+
+//File I/O
+void DiskSave(char filename[], int source, int length);
+void DiskLoad(char filename[], int dest);
+
+//Input
+void InitializeInput();
+void UpdateInput();
+bool InputChanged();
+bool NoInput();
+bool InputUp();
+bool InputDown(); 
+bool InputLeft();
+bool InputRight();
+bool InputFire();
+
+
+#endif
