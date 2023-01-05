51b29a33c5f5871741af6b096d1c99a7ac4cfffb
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-01-05 11:10:15 -0500

    updated from 8bitworkshop.com

--- XINAGA.c
+++ XINAGA.c
@@ -9,11 +9,17 @@ void main()
   byte i = 0;
   InitializeGraphics();  
 
-
+  DrawBorder("Character Set@", 0, 0, 18, 18, false);
+  DrawBorder("Tiles@", 18, 0, 18, 18, false);
+  #if defined(__APPLE2__)
+  DrawBorder("Hello Apple!@", 0, ROWS - 3, 16, 2, false);
+  #endif
+  #if defined(__C64__)
+  DrawBorder("Hello C64!@", 0, ROWS - 3, 16, 3, false);
+  #endif
   while (1)
   {
-    DrawBorder("Character Set@", 0, 0, 18, 18, false);
-    DrawBorder("Tiles@", 18, 0, 18, 18, false);
+    
     for (y= 0; y < 16; ++y)
       for (x = 0; x < 16; ++x)
       {
@@ -23,8 +29,8 @@ void main()
     CopyBufferArea(1, 1, 16, 16);
     //CopyBuffer();
     i += 16;
-
-
+    
+    z = i;
     SetTileOrigin(19, 1);
     for (y = 8; y != 0; --y)
       for (x = 8; x != 0; --x)
@@ -33,12 +39,7 @@ void main()
         ++z;
       }
     
-    #if defined(__APPLE2__)
-    DrawBorder("Hello Apple!@", 0, ROWS - 3, 16, 2, false);
-    #endif
-    #if defined(__C64__)
-    DrawBorder("Hello C64!@", 0, ROWS - 3, 16, 3, false);
-    #endif
+    
     //ClearScreen();
   }
 }
