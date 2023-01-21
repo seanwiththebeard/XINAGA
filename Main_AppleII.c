9c96c834efc07dbc080bddc15e880c9be11a46ef
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-01-21 18:58:02 -0500

    updated from 8bitworkshop.com

---
+++ Main_AppleII.c
@@ -0,0 +1,16 @@
+#include "Common.h"
+#include "xinaga.h"
+//#link "Demo.c"
+
+//#link "Graphics.c"
+//#link "Common.c"
+#define CFGFILE apple2-hgr2.cfg
+//#resource "apple2-hgr2.cfg"
+
+
+
+void main()
+{
+  Demo();
+}
+
