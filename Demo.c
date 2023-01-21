520c867f8ef63f0eb2abfb9f039978415ef7ed89
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-01-21 18:57:25 -0500

    updated from 8bitworkshop.com

---
+++ Demo.c
@@ -0,0 +1,128 @@
+#include "xinaga.h"
+#include "Common.h"
+
+//#link "Graphics.c"
+//#link "Common.c"
+//#define CFGFILE apple2-hgr2.cfg
+//#resource "apple2-hgr2.cfg"
+
+//Tests
+void DrawCharset()
+{
+  byte x, y;
+  byte i = 0;
+  DrawBorder("Character Set@", 0, 0, 18, 18, false);
+  
+  for (y= 0; y < 16; ++y)
+    for (x = 0; x < 16; ++x)
+    {
+      SetChar(i, x+1, y+1);
+      ++i;
+    }
+  i += 16;
+}
+
+void DrawTiles()
+{
+  byte x, y, z;
+  DrawBorder("Tiles@", 18, 0, 18, 18, false);
+  SetTileOrigin(19, 1);
+  StoreBuffer();
+  for (y = 0; y < 8; ++y)
+    for (x = 0; x < 8; ++x)
+    {
+      DrawTileFast(/*x*y - */z, x, y);
+      ++z;
+    }
+    SwapBuffer();
+}
+
+void DrawMap()
+{
+  #define sizeX 9
+  #define sizeY 9
+  byte MapData[sizeX * sizeY];
+  byte MapTemp[sizeX * sizeY];
+  byte charX, charY, charI;
+  byte x, y, z, i = 0;
+  DrawBorder("Map@", 0, 0, sizeX * 2 + 2, sizeY * 2 + 2, false);
+  for (x = 0; x < (sizeX * sizeY); ++x)
+    MapData[x] = 36;
+  MapData[36] = 47;
+  MapData[26] = 44;
+  //MapData[10] = 2;
+  SetTileOrigin(1, 1);
+  
+  charX = 2;
+  charY = 0;
+  charI = 2;
+  while(1)
+  {
+    memcpy(&MapTemp[0], &MapData[0], sizeX * sizeY);
+    MapTemp[charY*sizeX + charX] = charI;
+    if (i & 1)
+      charX++;
+    else
+      charY++;
+    ++i;
+    
+    if (charX > sizeX - 1)
+      charX -= sizeX;
+    if (charY > sizeY - 1)
+      charY -= sizeY;
+    
+    z = 0;
+    StoreBuffer();
+    for (y = 0; y < sizeY; ++y)
+      for (x = 0; x < sizeX; ++x)
+      {        
+        DrawTileFast(MapTemp[z], x, y);
+        ++z;
+    }
+    SwapBuffer();
+    
+    z = 2;
+    for (y = 0; y < sizeY; ++y)
+      for (x = 0; x < sizeX; ++x)
+      {        
+        //MapData[z - 1] = MapData[z];
+        ++z;
+    }
+  }
+}
+
+/*void ScrollScreen()
+{
+    Scroll(right);
+    Scroll(right);
+    Scroll(down);
+    Scroll(down);
+    Scroll(left);
+    Scroll(left);
+    Scroll(up);
+    Scroll(up);
+}*/
+
+void Demo()
+{
+  InitializeGraphics();
+  ClearScreen();
+  
+  #if defined(__APPLE2__)
+  DrawBorder("Hello Apple!@", 0, ROWS - 3, COLS, 2, false);
+  #endif
+  
+  #if defined(__C64__)
+  DrawBorder("Hello C64!@", 0, ROWS - 3, COLS, 3, false);
+  #endif
+  
+  while(1)
+  {
+    DrawCharset();
+    DrawTiles();
+    //InvertScreen();
+    //InvertCharacterSet();
+    DrawMap();
+    //ScrollScreen();
+  }
+}
---
+++ Main_C64.c
@@ -0,0 +1,11 @@
+#include "Common.h"
+#include "xinaga.h"
+//#link "Demo.c"
+
+//#link "Graphics.c"
+//#link "Common.c"
+
+void main()
+{
+  Demo();
+}
