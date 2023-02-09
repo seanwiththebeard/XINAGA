2fa629ba081da61dd8dffd37e7b82b226a56c59f
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-02-09 14:00:13 -0500

    updated from 8bitworkshop.com

--- Main_AppleII.c
+++ Main_AppleII.c
@@ -1,3 +1,8 @@
+#if defined(__APPLE2__)
+//#resource "apple2-xinaga.cfg"
+#define CFGFILE apple2-xinaga.cfg
+#endif
+
 #include "xinaga.h"
 //#link "Graphics.c"
 //#link "Input.c"
@@ -8,12 +13,6 @@
 
 //#link "System_MessageWindow.c"
 #include "System_MessageWindow.h"
-
-
-#if defined(__APPLE2__)
-//#resource "apple2-xinaga.cfg"
-#define CFGFILE apple2-xinaga.cfg
-#endif
 
 void Demo(void);
 void main()
