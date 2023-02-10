13bbe0adf395c1dbe40b0942033644d9ce83df0c
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-02-10 10:54:44 -0500

    updated from 8bitworkshop.com

--- Main_AppleII.c
+++ Main_AppleII.c
@@ -1,17 +1,16 @@
-#if defined(__APPLE2__)
-//#resource "apple2-xinaga.cfg"
-#define CFGFILE apple2-xinaga.cfg
-#endif
-
 #include "xinaga.h"
+//#link "Game_Demo.c"
+//#link "Game_Screen_Map.c"
+
 //#link "XINAGA_Graphics.c"
 //#link "XINAGA_Input.c"
 //#link "XINAGA_FileIO.c"
 //#link "XINAGA_Console.c"
 
-//#link "Game_Demo.c"
-//#link "Game_Screen_Map.c"
-
+#if defined(__APPLE2__)
+//#resource "apple2-xinaga.cfg"
+#define CFGFILE apple2-xinaga.cfg
+#endif
 
 void Demo(void);
 void main()
