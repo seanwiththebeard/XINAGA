b62a73f00cbeb1d6015c86b4c6abed6b27f17e04
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2023-05-16 07:57:31 -0400

    updated from 8bitworkshop.com

--- Game_Screen_MapGen.c
+++ Game_Screen_MapGen.c
@@ -13,6 +13,20 @@
 #define water 0x84
 byte map[height][width];
 
+/* World Seed Parameters
+(Eight flags building a byte)
+Night / Day 	//128
+North / South	//64
+East / West	//32
+Before / After	//16
+		//8
+                //4
+                //2
+                //1
+
+
+*/
+
 typedef struct vector2
 {
   char x, y;
@@ -132,9 +146,14 @@ void clearPoints()
 
 void clampPoint(struct vector2 *clmpt)
 {
-  if (clmpt->x >= width)
-    clmpt->x = 0;
-  if (clmpt->y >= height)
+  if (clmpt->x == 255)
+    clmpt->x = width - 1;
+  if (clmpt->y == 255)
+    clmpt->y = height - 1;
+  
+  if (clmpt->x == width)
+    clmpt->x == 0;
+  if (clmpt->y == height)
     clmpt->y = 0;
 }
 void checkLandlocked()
@@ -171,10 +190,10 @@ void checkLandlocked()
 
 byte countAdjacent(byte index, byte x, byte y)
 {
-  byte i;
+  byte i = 0;
   //for (i = 0; i < CountPoints(); ++i)
   struct vector2 north, south, east, west;
-  
+
   north.x = x;
   north.y = y - 1;
   south.x = x;
@@ -200,63 +219,103 @@ byte countAdjacent(byte index, byte x, byte y)
   return i;
 }
 
-void GenerateMap(byte seed)
+void FillAdjacent(byte passes)
 {
-  byte x, y, failures = 0;
-
-
-  clearPoints();
-
-  sprintf(strTemp, "Seed (%d)@", seed);
-  WriteLineMessageWindow(strTemp, 0);
-
-  for (y = 0; y < height; ++y)
-    for (x = 0; x < width; ++x)
-    {
-      if (x % 2 == 0)
-        map[y][x] = water;
-      else
-        map[y][x] = water + 1;
-
-      SetChar(map[y][x], posX + x, posY + y);
-    }
-  srand(seed);
-  for (x = 0; x < pointsCount; ++x)
+  byte x, y, i;
+  for (i = 0; i < passes; ++i)
   {
-    byte h = rand() % height;
-    byte w = rand() % width;
-
-    while (checkPoints(grass, w, h))
-    {
-      h = rand() % height;
-      w = rand() % width;
-      failures++;
-    }
-    createPoint(w, h);
-    map[h][w] = grass;
-    SetChar(grass, posX + w, posY + h);
-
-    sprintf(strTemp, "Count (%d)@", x);
+    for (y = 0; y < height; ++y)
+      for (x = 0; x < width; ++x)
+      {
+        if (map[y][x] != grass)
+          if (countAdjacent(grass, x, y) > 1)
+          {
+            createPoint(x, y);
+            map[y][x] = grass;
+            SetChar(grass, posX + x, posY + y);
+            sprintf(strTemp, "Filled (%d, %d)@", x, y);
+            WriteLineMessageWindow(strTemp, 0);
+          }
+      }
+    sprintf(strTemp, "Pass %d done@", i + 1);
     WriteLineMessageWindow(strTemp, 0);
   }
+}
 
-  sprintf(strTemp, "failures (%d)@", failures);
-  WriteLineMessageWindow(strTemp, 0);
-
-  //checkLandlocked();
-  
+void RemoveIslands()
+{
+  byte x, y;
   for (y = 0; y < height; ++y)
     for (x = 0; x < width; ++x)
     {
-      if (countAdjacent(grass, x, y) > 2)
-      {
-        map[y][x ] = grass;
-        sprintf(strTemp, "filled (%d, %d)@", x, y);
-        WriteLineMessageWindow(strTemp, 0);
-      }
+      if (map[y][x] == grass)
+        if (countAdjacent(grass, x, y) == 0)
+        {
+          map[y][x] = water;
+          SetChar(water, posX + x, posY + y);
+          sprintf(strTemp, "Removed (%d, %d)@", x, y);
+          WriteLineMessageWindow(strTemp, 0);
+        }
     }
-    
-  /*for (x = 0; x < pointsCount; ++x)
+  sprintf(strTemp, "Islands Removed@");
+  WriteLineMessageWindow(strTemp, 0);
+}
+
+  void GenerateMap(byte seed)
+  {
+    byte x, y, failures = 0;
+
+
+    clearPoints();
+
+    sprintf(strTemp, "Seed (%d)@", seed);
+    WriteLineMessageWindow(strTemp, 0);
+
+    for (y = 0; y < height; ++y)
+      for (x = 0; x < width; ++x)
+      {
+        if (x % 2 == 0)
+          map[y][x] = water;
+        else
+          map[y][x] = water + 1;
+
+        SetChar(map[y][x], posX + x, posY + y);
+      }
+    srand(seed);
+    for (x = 0; x < pointsCount; ++x)
+    {
+      byte h = rand() % height;
+      byte w = rand() % width;
+
+      while (checkPoints(grass, w, h))
+      {
+        h = rand() % height;
+        w = rand() % width;
+        failures++;
+      }
+      createPoint(w, h);
+      map[h][w] = grass;
+      SetChar(grass, posX + w, posY + h);
+
+      sprintf(strTemp, "Count (%d)@", x);
+      WriteLineMessageWindow(strTemp, 0);
+    }
+
+    sprintf(strTemp, "failures (%d)@", failures);
+    WriteLineMessageWindow(strTemp, 0);
+
+    //checkLandlocked();
+
+    FillAdjacent(5);
+    RemoveIslands();
+
+    while(1)
+    {
+      UpdateInput();
+      if (InputFire())
+        break;
+    }
+    /*for (x = 0; x < pointsCount; ++x)
   {
 
     if (getPoint(x)->x % 2 == 0)
@@ -270,36 +329,36 @@ void GenerateMap(byte seed)
     {
       SetChar(map[y][x], posX + x, posY + y);
     }*/
-}
-
-screenName Update_MapGen()
-{
-  byte seed  = 0;
-  screenName nextScreen = Title;
-  bool exit = false;
-  DrawBorder("Map Generator@",posX - 1, posY - 1, width + 2, height + 2, true);
-  while(1)
-  {
-    GenerateMap(seed);
-    ++seed;
   }
-  while (!exit)
+
+  screenName Update_MapGen()
   {
-    UpdateInput();
-    if (InputChanged())
+    byte seed  = 0;
+    screenName nextScreen = Title;
+    bool exit = false;
+    DrawBorder("Map Generator@",posX - 1, posY - 1, width + 2, height + 2, true);
+    while(1)
     {
-      if (InputUp())
+      GenerateMap(seed);
+      ++seed;
+    }
+    while (!exit)
+    {
+      UpdateInput();
+      if (InputChanged())
       {
-        ++seed;
-        GenerateMap(seed);
-        //exit = true;
-      }
-      if (InputFire())
-      {
-        exit = true;
+        if (InputUp())
+        {
+          ++seed;
+          GenerateMap(seed);
+          //exit = true;
+        }
+        if (InputFire())
+        {
+          exit = true;
+        }
       }
     }
-  }
 
-  return nextScreen;
-}
+    return nextScreen;
+  }
