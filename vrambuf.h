9d2bf66f37e9f013ed104eb691e0560c09501a75
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2024-08-01 15:26:04 -0400

    updated from 8bitworkshop.com

--- vrambuf.h
+++ vrambuf.h
@@ -5,7 +5,7 @@
 #include "neslib.h"
 
 // VBUFSIZE = maximum update buffer bytes
-#define VBUFSIZE 84
+#define VBUFSIZE 80
 
 // update buffer starts at $100 (stack page)
 #define updbuf ((byte*)0x100)
