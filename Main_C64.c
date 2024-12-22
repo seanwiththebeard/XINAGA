71169f14800fd59c5d7e9ab613239607eef515d4
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2024-12-22 16:53:58 -0500

    updated from 8bitworkshop.com

--- Main_C64.c
+++ Main_C64.c
@@ -1,22 +1,12 @@
-#if defined(__C64__)
-//#resource "C64.cfg"
-//#resource "C64.ctm"
-//#resource "1bitchars.ctm"
-#define CFGFILE C64.cfg
-#endif
-
 #include "Xinaga.h"
 #include "GameData.h"
-
 //#link "XINAGA_Graphics.c"
 //#link "XINAGA_Input.c"
 //#link "XINAGA_FileIO.c"
 //#link "XINAGA_Audio.c"
 //#link "XINAGA_Console.c"
-
 //#link "Game_Demo.c"
 //#link "Game_System.c"
-
 //#link "Game_Screen_Title.c"
 //#link "Game_Screen_AddCharacter.c"
 //#link "Game_Screen_Map.c"
@@ -24,15 +14,14 @@
 //#link "Game_Screen_MapGen.c"
 //#link "Game_Screen_Scenario.c"
 
-//void Demo(void);
+#if defined(__C64__)
+//#resource "C64.cfg"
+//#resource "C64.ctm"
+//#resource "1bitchars.ctm"
+#define CFGFILE C64.cfg
+#endif
+
 void main(void)
 {
-  //byte x, y;
-  //InitializeGraphics();
-  
-  //for (y = 0; y < 16; ++y)
-   //for (x = 0; x < 16; ++x)
-      //SetChar(x + 16*y,x, y);
-  //while (1);
   Demo();
 }
