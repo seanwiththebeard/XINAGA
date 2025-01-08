5bddc1b1b8ccb3307f18cdd8f2d716200f7fcdf1
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2025-01-07 22:16:20 -0500

    updated from 8bitworkshop.com

--- Main_MSX.c
+++ Main_MSX.c
@@ -6,11 +6,11 @@
 //#link "XINAGA_FileIO.c"
 //#link "XINAGA_Audio.c"
 //#link "XINAGA_Console.c"
-//#link "Game_Demo.c"
-//#link "Game_System.c"
+////#link "Game_Demo.c"
+////#link "Game_System.c"
 ////#link "Game_Screen_Title.c"
 ////#link "Game_Screen_AddCharacter.c"
-//#link "Game_Screen_Map.c"
+////#link "Game_Screen_Map.c"
 ////#link "Game_Screen_Combat.c"
 ////#link "Game_Screen_MapGen.c"
 ////#link "Game_Screen_Scenario.c"
@@ -25,6 +25,9 @@
 //typedef uint8_t byte;
 //typedef uint16_t word;
 //typedef uint8_t bool;
+
+byte strTemp[ConsoleBufferLength];
+uint16_t randseed;
 
 void main(void)
 {
@@ -45,7 +48,7 @@ void main(void)
   WriteLineMessageWindow("Hello@", 0);
   while (1)
   {
-  WriteLineMessageWindow("Hello@", 0);
+  //WriteLineMessageWindow("Hello@", 0);
     
     //for (y = 0; y < 16; ++y)
       //for (x = 0; x < 16; ++x)
