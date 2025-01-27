b89d88ecad6ac0170f09d0909d0488fd4001c9fc
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2025-01-27 12:37:36 -0500

    updated from 8bitworkshop.com

---
+++ Main_Atari800.c
@@ -0,0 +1,20 @@
+#include "Xinaga.h"
+#include "GameData.h"
+//#link "XINAGA_Graphics.c"
+//#link "XINAGA_Input.c"
+//#link "XINAGA_FileIO.c"
+//#link "XINAGA_Audio.c"
+//#link "XINAGA_Console.c"
+//#link "Game_Demo.c"
+//#link "Game_System.c"
+//#link "Game_Screen_Title.c"
+//#link "Game_Screen_AddCharacter.c"
+//#link "Game_Screen_Map.c"
+//#link "Game_Screen_Combat.c"
+//#link "Game_Screen_MapGen.c"
+//#link "Game_Screen_Scenario.c"
+
+void main(void)
+{
+  Demo();
+}
