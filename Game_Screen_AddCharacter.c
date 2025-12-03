4979fdf9c0769b1ed57e8bdbdf42d9e2ff1cd243
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2025-12-03 14:12:17 -0500

    updated from 8bitworkshop.com

--- Game_Screen_AddCharacter.c
+++ Game_Screen_AddCharacter.c
@@ -8,8 +8,8 @@
 #if defined (__NES__)
 #pragma code-name (push, "ADDCHAR")
 #pragma rodata-name (push, "ADDCHAR")
-#pragma data-name (push, "XRAM")
-#pragma bss-name (push, "XRAM")
+//#pragma data-name (push, "XRAM")
+//#pragma bss-name (push, "XRAM")
 #endif
 
 screenName nextScreen;
