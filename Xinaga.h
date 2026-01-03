edcfc170180c8668954049e06210c883fd8d813c
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2026-01-02 19:01:14 -0500

    updated from 8bitworkshop.com

--- Xinaga.h
+++ Xinaga.h
@@ -1,11 +1,80 @@
 #ifndef _XINAGA_H
 #define _XINAGA_H
 
-#if defined(__C64__)
-#include <cbm_petscii_charmap.h>
-//#include <cbm_screen_charmap.h>
-//#include <ascii_charmap.h>
+#include <string.h> //For memcpy
+#include <stdint.h> //For byte and sbyte
+#include <stdlib.h> //For srand
+typedef uint8_t byte;
+typedef int8_t sbyte;	// 8-bit signed
+#define bool byte
+#define false 0
+#define true 1
+#define direction byte
+#define up 0
+#define down 1
+#define left 2
+#define right 3
 
+extern uint16_t randseed;
+byte ReadBit(byte byteToRead, char bit);
+
+//Screen Layout
+#if defined(__APPLE2__)
+#define COLS 40
+#define ROWS 24
+#define consolePosX 1
+#define consolePosY 2 + 2*viewportHeight
+#define consoleWidth 38
+#define consoleHeight (ROWS - viewportHeight * 2 - 3)
+#define selectionPosX consoleWidth + 2
+#define selectionPosY consolePosY
+#define selectionWidth 6
+#define selectionHeight consoleHeight
+#define selectionCount 4
+#define viewportPosX 1
+#define viewportPosY 1
+#define viewportWidth 9
+#define viewportHeight 9
+#define contextMenuPosX viewportPosX + viewportWidth * 2 + 1
+#define contextMenuPosY viewportPosY
+#define contextMenuWidth (COLS - (viewportWidth * 2) - 3)
+#define contextMenuHeight (2 * viewportHeight)
+
+#include <conio.h> //for cgetc() and kbhit()
+#define STROBE(addr) __asm__ ("sta %w", addr)
+#pragma warn (remap-zero, push, off)
+#pragma charmap ('^', 0x7E)
+#pragma warn (remap-zero, pop)
+extern char HGR[0x2000];
+extern unsigned int RowsHGR[192];
+extern const byte const charset[2048];
+extern const byte const attributeset[256];
+extern byte* CharRam;
+void A2Pixel(byte x, byte y, byte color);
+#endif
+
+#if defined(__C64__)
+#define COLS 40
+#define ROWS 25
+#define consolePosX 1
+#define consolePosY 2 + 2*viewportHeight
+#define consoleWidth 38
+#define consoleHeight (ROWS - viewportHeight * 2 - 3)
+#define selectionPosX consoleWidth + 2
+#define selectionPosY consolePosY
+#define selectionWidth 6
+#define selectionHeight consoleHeight
+#define selectionCount 4
+#define viewportPosX 1
+#define viewportPosY 1
+#define viewportWidth 9
+#define viewportHeight 9
+#define contextMenuPosX viewportPosX + viewportWidth * 2 + 1
+#define contextMenuPosY viewportPosY
+#define contextMenuWidth (COLS - (viewportWidth * 2) - 3)
+#define contextMenuHeight (2 * viewportHeight)
+
+#include <cbm_petscii_charmap.h>
 #pragma charmap(0x41, 0x61)
 #pragma charmap(0x42, 0x62)
 #pragma charmap(0x43, 0x63)
@@ -60,90 +129,16 @@
 #pragma charmap(0x79, 0x59)
 #pragma charmap(0x7a, 0x5a)
 
-
-//#include <c64.h>
-//#include "C64Charmap.h"
-//#pragma local-strings(push, on)
-#endif
-
-
-#include <string.h> //For memcpy
-#include <stdint.h> //For byte and sbyte
-int sprintf (char* buf, const char* format, ...);
-//#include <stdio.h> //For sprintf
-#include <stdlib.h> //For srand
-typedef uint8_t byte;
-typedef int8_t sbyte;	// 8-bit signed
-
-
-//Screen Layout
-#if defined(__APPLE2__)
-#define COLS 40
-#define ROWS 24
-#define consolePosX 1
-#define consolePosY 2 + 2*viewportHeight
-#define consoleWidth 38
-#define consoleHeight (ROWS - viewportHeight * 2 - 3)
-#define selectionPosX consoleWidth + 2
-#define selectionPosY consolePosY
-#define selectionWidth 6
-#define selectionHeight consoleHeight
-#define selectionCount 4
-#define viewportPosX 1
-#define viewportPosY 1
-#define viewportWidth 9
-#define viewportHeight 9
-#define contextMenuPosX viewportPosX + viewportWidth * 2 + 1
-#define contextMenuPosY viewportPosY
-#define contextMenuWidth (COLS - (viewportWidth * 2) - 3)
-#define contextMenuHeight (2 * viewportHeight)
-#endif
-
-#if defined(__ATARI__)
-#define COLS 40
-#define ROWS 24
-#define consolePosX 1
-#define consolePosY 2 + 2*viewportHeight
-#define consoleWidth 38
-#define consoleHeight (ROWS - viewportHeight * 2 - 3)
-#define selectionPosX consoleWidth + 2
-#define selectionPosY consolePosY
-#define selectionWidth 6
-#define selectionHeight consoleHeight
-#define selectionCount 4
-#define viewportPosX 1
-#define viewportPosY 1
-#define viewportWidth 11
-#define viewportHeight 9
-#define contextMenuPosX viewportPosX + viewportWidth * 2 + 1
-#define contextMenuPosY viewportPosY
-#define contextMenuWidth (COLS - (viewportWidth * 2) - 3)
-#define contextMenuHeight (2 * viewportHeight)
-#endif
-
-#if defined(__C64__)
-#define COLS 40
-#define ROWS 25
-#define consolePosX 1
-#define consolePosY 2 + 2*viewportHeight
-#define consoleWidth 38
-#define consoleHeight (ROWS - viewportHeight * 2 - 3)
-#define selectionPosX consoleWidth + 2
-#define selectionPosY consolePosY
-#define selectionWidth 6
-#define selectionHeight consoleHeight
-#define selectionCount 4
-#define viewportPosX 1
-#define viewportPosY 1
-#define viewportWidth 9
-#define viewportHeight 9
-#define contextMenuPosX viewportPosX + viewportWidth * 2 + 1
-#define contextMenuPosY viewportPosY
-#define contextMenuWidth (COLS - (viewportWidth * 2) - 3)
-#define contextMenuHeight (2 * viewportHeight)
+extern byte characterset[2048];
+extern byte attributeset[256];
+extern byte* CharRam;
+void raster_wait(byte line);
+void SetBorder(byte color);
+void SetBG(byte color);
 #endif
 
 #if defined(__NES__)
+#include <peekpoke.h>
 #define COLS 32
 #define ROWS 30
 #define consolePosX 2
@@ -163,6 +158,33 @@ typedef int8_t sbyte;	// 8-bit signed
 #define contextMenuPosY viewportPosY
 #define contextMenuWidth (COLS - (viewportWidth * 2) - 5)
 #define contextMenuHeight (2 * viewportHeight)
+#define MMC_MODE 0x00
+#define MMC3_SET_REG(r,n)\
+	POKE(0x8000, MMC_MODE|(r));\
+	POKE(0x8001, (n));
+#define MMC3_CHR_0000(n) MMC3_SET_REG(0,n)
+#define MMC3_CHR_0800(n) MMC3_SET_REG(1,n)
+#define MMC3_CHR_1000(n) MMC3_SET_REG(2,n)
+#define MMC3_CHR_1400(n) MMC3_SET_REG(3,n)
+#define MMC3_CHR_1800(n) MMC3_SET_REG(4,n)
+#define MMC3_CHR_1C00(n) MMC3_SET_REG(5,n)
+#define MMC3_PRG_8000(n) MMC3_SET_REG(6,n)
+#define MMC3_PRG_A000(n) MMC3_SET_REG(7,n)
+#define MMC3_MIRROR(n) POKE(0xa000, (n))
+//; WRAM_OFF $40
+//; WRAM_ON $80
+//; WRAM_READ_ONLY $C0
+#define MMC3_WRAM_DISABLE() POKE(0xA001, 0x40)
+#define MMC3_WRAM_ENABLE() POKE(0xA001, 0x80)
+#define MMC3_WRAM_READ_ONLY() POKE(0xA001, 0xC0)
+extern byte ScreenChars[ROWS*COLS];
+extern byte attributeset[256];
+extern const int ScreenCharSize;
+extern char ATTRIBUTE_TABLE[0x40];
+extern const char PALETTE_0[16];
+extern const char PALETTE_1[16];
+extern const char PALETTE_2[16];
+extern const char PALETTE_3[16];
 #endif
 
 #if defined(MSX)
@@ -187,88 +209,6 @@ typedef int8_t sbyte;	// 8-bit signed
 #define contextMenuHeight (2 * viewportHeight)
 #endif
 
-#if defined(__APPLE2__)
-#include <conio.h> //for cgetc() and kbhit()
-
-#define STROBE(addr) __asm__ ("sta %w", addr)
-#pragma warn (remap-zero, push, off)
-#pragma charmap ('^', 0x7E)
-#pragma warn (remap-zero, pop)
-
-extern char HGR[0x2000];
-extern unsigned int RowsHGR[192];
-#endif
-
-#if defined (__NES__)
-#include <peekpoke.h>
-
-
-#define MMC_MODE 0x00
-
-#define MMC3_SET_REG(r,n)\
-	POKE(0x8000, MMC_MODE|(r));\
-	POKE(0x8001, (n));
-
-#define MMC3_CHR_0000(n) MMC3_SET_REG(0,n)
-#define MMC3_CHR_0800(n) MMC3_SET_REG(1,n)
-#define MMC3_CHR_1000(n) MMC3_SET_REG(2,n)
-#define MMC3_CHR_1400(n) MMC3_SET_REG(3,n)
-#define MMC3_CHR_1800(n) MMC3_SET_REG(4,n)
-#define MMC3_CHR_1C00(n) MMC3_SET_REG(5,n)
-#define MMC3_PRG_8000(n) MMC3_SET_REG(6,n)
-#define MMC3_PRG_A000(n) MMC3_SET_REG(7,n)
-
-#define MMC3_MIRROR(n) POKE(0xa000, (n))
-
-//; WRAM_OFF $40
-//; WRAM_ON $80
-//; WRAM_READ_ONLY $C0
-#define MMC3_WRAM_DISABLE() POKE(0xA001, 0x40)
-#define MMC3_WRAM_ENABLE() POKE(0xA001, 0x80)
-#define MMC3_WRAM_READ_ONLY() POKE(0xA001, 0xC0)
-
-
-//#pragma bss-name ("XRAM")
-
-extern byte ScreenChars[ROWS*COLS];
-extern byte attributeset[256];
-extern const int ScreenCharSize;
-
-extern char ATTRIBUTE_TABLE[0x40];
-extern const char PALETTE_0[16];
-extern const char PALETTE_1[16];
-extern const char PALETTE_2[16];
-extern const char PALETTE_3[16];
-#endif
-
-
-//typedef enum { false, true } bool;
-#define bool byte
-#define false 0
-#define true 1
-//typedef enum { up, down, left, right } direction;
-#define direction byte
-#define up 0
-#define down 1
-#define left 2
-#define right 3
-
-extern uint16_t randseed;
-byte ReadBit(byte byteToRead, char bit);
-
-#if defined (__C64__)
-extern byte characterset[2048];
-extern byte attributeset[256];
-extern byte* CharRam;
-#endif
-
-#if defined (__APPLE2__)
-extern const byte const charset[2048];
-extern const byte const attributeset[256];
-extern byte* CharRam;
-void A2Pixel(byte x, byte y, byte color);
-#endif
-
 //Graphics
 //	System
 #define ColorBG 0
@@ -276,47 +216,38 @@ void A2Pixel(byte x, byte y, byte color);
 #define ColorText 1
 void InitializeGraphics(void);
 void wait_vblank(byte frames);
-
 void SetAttrib(byte x, byte y, byte pal, bool direct);
 void UpdateAttributes(void);
 void ScreenFadeOut(void);
 void ScreenFadeIn(void);
 
-#if defined(__C64__)
-void raster_wait(byte line);
-void SetBorder(byte color);
-void SetBG(byte color);
-#endif
-
 extern int YColumnIndex[ROWS];
 
 //	Drawing
-void ClearScreen(void);
-//void SetChar();
 extern byte SetCharIndex;
 extern byte SetCharX;
 extern byte SetCharY;
 void _SetChar(void);
 #define SetChar(index, x, y) do {SetCharIndex = (index); SetCharX = (x); SetCharY = (y); _SetChar();}while(0)
+void ClearScreen(void);
 void SetColor(byte x, byte y, byte index);
-byte GetChar(byte x, byte y);
 void DrawLineH(byte index, byte x, byte y, byte length);
 void DrawLineV(byte index, byte x, byte y, byte length);
 void DrawBorder(char *text, byte xPos, byte yPos, byte width, byte height, bool fill);
 void DrawCharset(void);
+byte GetChar(byte x, byte y);
 
 //	Tiles
-void SetTileOrigin(byte x, byte y);
+#define TileCount 64
+#define TileSize 16
 extern byte DrawTileX;
 extern byte DrawTileY;
 extern byte DrawTileIndex;
 extern byte DrawTilePalette;
-#define TileCount 64
-#define TileSize 16
 extern byte tilesBlocked[TileCount];
 extern byte tilesOpaque[TileCount];
 extern byte tilesPalette[TileCount];
-
+void SetTileOrigin(byte x, byte y);
 void DrawTileBuffer(bool drawChars);
 void DrawTileDirect(void);
 void DrawTileDirectXY(byte index, byte x, byte y);
@@ -326,9 +257,6 @@ void FillViewport(byte index, byte width, byte height);
 void ReadyArrow(byte x, byte y);
 void DrawArrow(byte x, byte y);
 void ClearArrow(void);
-
-//	Text
-void PrintString(char *text, byte posx, byte posy, bool fast, bool buffer);
 
 //	Buffer
 void SetBuffer(bool value);
@@ -357,15 +285,17 @@ bool InputLeft(void);
 bool InputRight(void);
 bool InputFire(void);
 
-//Console
-void ResizeMessageWindow (byte x, byte y, byte w, byte h);
+//	Text
+int sprintf (char* buf, const char* format, ...);
+void PrintString(char *text, byte posx, byte posy, bool fast, bool buffer);
 
-void WriteLineMessageWindow(char *message, byte delay);
-void SetLineMessageWindow(char *message, byte delay);
-extern const char* const Messages[];
-void DrawCharStats(void);
+//Console
 #define ConsoleBufferLength 128
 extern byte strTemp[ConsoleBufferLength];
+extern const char* const Messages[];
+void ResizeMessageWindow (byte x, byte y, byte w, byte h);
+void WriteLineMessageWindow(char *message, byte delay);
+void SetLineMessageWindow(char *message, byte delay);
 void ConsoleBufferReset(void);
 void ConsoleBufferAdd(char *message);
 void ConsoleBufferBackspace(void);
@@ -373,14 +303,14 @@ void ConsoleBufferAddNumber(int value);
 void ConsoleBufferPrint(byte x, byte y);
 void ConsoleBufferPrintConsole(byte delay);
 void DrawConsoleContent(void);
-
+void DrawCharStats(void);
 
 //Selection Menu
 void ResetMenu(char *title, byte posX, byte posY, byte w, byte h, byte c);
 void SetMenuItem(byte index, char *value);
-byte GetMenuSelection(void);
 void HighlightMenuItem(byte index);
-byte IsMenuItemHighlighted(byte index);
 void DrawMenu(void);
 void ClearMenu(void);
+byte IsMenuItemHighlighted(byte index);
+byte GetMenuSelection(void);
 #endif
